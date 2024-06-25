04316b11efd3b9459187ce67e28bda8d8a414258
Kamesh Chaudhary <kameshchaudhary17@gmail.com>
2024-06-25 14:30:57 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const name = 'kamesh'
+const name1 = 'kamesh'
+const name2 = 'kamesh'
+
+
+
+//array
+const names = ['Kamesh Chaudhary', 'Messi', 'Ronaldo']
+names.push('hello')
+names.pop()
+names.shift()
+names.unshift()
+
+console.log(names[2])
+
+//object
+const me = {
+    name: 'Kamesh',
+    age: 22,
+    address: 'duhabi'
+}
+
+console.log(me.age)
+
+me.faculty = 'BIT'
