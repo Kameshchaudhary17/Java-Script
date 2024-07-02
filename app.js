3ad6486431d15bb3ef2cb6880a7c63e4c5080d09
Kamesh Chaudhary <kameshchaudhary17@gmail.com>
2024-07-02 22:31:17 +0545

    revise JS

--- app.js
+++ app.js
@@ -3,23 +3,29 @@ const name1 = 'kamesh'
 const name2 = 'kamesh'
 
 
-
+console.log("hello")
 //array
 const names = ['Kamesh Chaudhary', 'Messi', 'Ronaldo']
-names.push('hello')
-names.pop()
-names.shift()
-names.unshift()
-
 console.log(names[2])
+// names.push('hello')
+// names.pop()
+// names.shift()
+// names.unshift()
+
+// console.log(names[2])
+
+
+const a = 9
+const b = 10
+console.log(a+b)
 
 //object
-const me = {
-    name: 'Kamesh',
-    age: 22,
-    address: 'duhabi'
-}
+// const me = {
+//     name: 'Kamesh',
+//     age: 22,
+//     address: 'duhabi'
+// }
 
-console.log(me.age)
+// console.log(me.age)
 
-me.faculty = 'BIT'
+// me.faculty = 'BIT'
---
+++ script.js
@@ -0,0 +1,33 @@
+
+//variable declare
+let name = "kamesh"
+
+var name1 = 3
+
+const name2 = "sandhya"
+
+console.log("my name is")
+
+
+
+const arrays = ["messi", "ronaldo", "neymar"]
+
+console.log(arrays[1])
+arrays.shift()
+arrays.push(10)
+arrays.pop()
+arrays.unshift(true)
+
+console.log(arrays)
+
+const messi = {
+    Name : "Leonel Messi",
+    Age : 37,
+    Location : "Argentina"
+}
+
+console.log(messi.Age)
+
+messi.Address = "Rosario"
+
+console.log(messi)
